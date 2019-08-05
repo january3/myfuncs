b85632d5b434f51754cbc646d691e2fb4db61fe0
January Weiner <january.weiner@gmail.com>
2019-08-05 20:08:57 +0200

    update

--- R/manuscript.R
+++ R/manuscript.R
@@ -3,8 +3,8 @@
 #' Smart counters for rmarkdown
 #' @param label character string consisting of a prefix, `_` (underscore) and actual label, e.g. `fig_label1`
 #' @export
-cc <- function(label) {
-  label <- as.character(substitute(label))
+cc <- function(label, asis=TRUE) {
+	if(asis) label <- as.character(substitute(label))
   prefix <- unlist(strsplit(label, "_"))[1]
   message(prefix)
   if(!exists("counters")) counters <<- list()
--- R/volcano.R
+++ R/volcano.R
@@ -1,10 +1,12 @@
 
+#' volcano plot
+#'
 #' @export
-volcano <- function(p, lfc, lfc.thr=1, p.thr=0.01, ylim=NULL) {
+volcano <- function(p, lfc, lfc.thr=1, p.thr=0.01, ylim=NULL, ...) {
 
   if(is.null(ylim)) ylim <- rev(range(p))
   plot(lfc, p, ylim=ylim, log="y", bty="n", pch=19, col="#33333333",
-    xlab="log(Fold)", ylab="FDR")
+    xlab="log(Fold)", ylab="FDR", ...)
 
   sel <- p < p.thr & abs(lfc) > lfc.thr
   points(lfc[sel], p[sel], pch=19, col="#cc000033")
